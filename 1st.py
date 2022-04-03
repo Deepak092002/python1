c36f49caf043ae5c66afebd6c4b76c6d8ecba577
Deepak092002 <92419644+Deepak092002@users.noreply.github.com>
2022-04-03 12:03:29 +0530

    Add files via upload

--- 1st.py
+++ 1st.py
@@ -1,8 +1,4 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 radius=float(input("Input the radius of the circle: "))
-Input the radius of the circle: 4.5
 pi=3.14
 Area=pi*radius*radius
 print("The area of the circle with radius",radius,"is",Area)
-The area of the circle with radius 4.5 is 63.585
---
+++ 2nd.py
@@ -0,0 +1,7 @@
+x=input("Input the Filename: ")
+if(x[-2:]=="py"):
+    print("The extension of the file is:'python'")
+if(x[-4:]=="java"):
+    print("The extension of the file is:Java")
+if(x[-3:]=="csv"):
+    print("the extension of the file is:CSV")
